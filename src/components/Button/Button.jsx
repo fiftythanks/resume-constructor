5eaa90a4aa83d45441af3faa429f5ca208b95d6d
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-06-09 00:06:12 +0700

    feat: create generic Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Button.scss';
+
+export default function Button({
+  children,
+  elements = [],
+  modifiers = [],
+  isDisabled = false,
+  isSubmit = false,
+  onClick,
+}) {
+  let className = 'Button';
+
+  // Add BEM element classes
+  if (Array.isArray(elements)) {
+    if (elements.length > 0) {
+      className += ` ${elements.join(' ')}`;
+    }
+  } else {
+    throw new TypeError('Elements must be provided wrapped in an array!');
+  }
+
+  // Add BEM modifier classes
+  if (Array.isArray(modifiers)) {
+    if (modifiers.length > 0) {
+      className += ` ${modifiers.join(' ')}`;
+
+      if (!modifiers.includes('_disabled') && isDisabled) {
+        className += ' _disabled';
+      }
+    }
+  } else {
+    throw new TypeError('Modifiers must be wrapped in an array!');
+  }
+
+  return (
+    <button
+      type={isSubmit ? 'submit' : 'button'}
+      onClick={onClick}
+      className={className}
+    >
+      {children}
+    </button>
+  );
+}
