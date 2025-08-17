b23093b73240bdb2d33770254a260e2920485093
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-08-17 16:14:40 +0700

    refactor(utils): check `joinItems()` input type

--- src/utils/joinItems.mjs
+++ src/utils/joinItems.mjs
@@ -1,3 +1,29 @@
+/**
+ * Because right now the function is hard to grasp just from looking
+ * at it. I haven't used it for a long time an it was hard to get
+ * what it was doing initially. And I don't even remember where it's
+ * used clearly.
+ */
+// TODO: rename `joinItems()` to something more telling, like `joinObjectItems()`.
 export default function joinItems(items) {
-  return items.map((item) => item.value).join(', ');
+  if (!Array.isArray(items)) {
+    throw new TypeError('Incorrect argument! joinItems() accepts arrays only.');
+  }
+
+  return items
+    .map((item) => {
+      if (
+        typeof item !== 'object' ||
+        item === null ||
+        item.value === 'undefined' ||
+        typeof item.value !== 'string'
+      ) {
+        throw new TypeError(
+          'Incorrect argument! Input array must consist of objects with properties "value" with string values.',
+        );
+      }
+
+      return item.value;
+    })
+    .join(', ');
 }
--- src/utils/joinItems.test.mjs
+++ src/utils/joinItems.test.mjs
@@ -10,4 +10,36 @@ describe('joinItems()', () => {
 
     expect(joinItems(items)).toBe('string, another string, third');
   });
+
+  it.each([
+    { value: 'string', type: 'string' },
+    { value: 5, type: 'number' },
+    { value: null, type: 'null' },
+    { value: undefined, type: 'undefined' },
+    { value: Symbol('string'), type: 'symbol' },
+    { value: true, type: 'boolean' },
+    { value: 151324314n, type: 'bigint' },
+    { value: { string: 'string' }, type: 'object' },
+    { value: function () {}, type: 'function' },
+  ])('throws TypeError if the argument is $type', ({ value }) => {
+    expect(() => joinItems(value)).toThrow(
+      TypeError('Incorrect argument! joinItems() accepts arrays only.'),
+    );
+  });
+
+  it.each([
+    { item: 'string', type: 'string' },
+    { item: 5, type: 'number' },
+    { item: null, type: 'null' },
+    { item: undefined, type: 'undefined' },
+    { item: Symbol('string'), type: 'symbol' },
+    { item: true, type: 'boolean' },
+    { item: 151324314n, type: 'bigint' },
+  ])('throws if the input array has $type elements', ({ item }) => {
+    expect(() => joinItems([{ value: 'string' }, item])).toThrow(
+      TypeError(
+        'Incorrect argument! Input array must consist of objects with properties "value" with string values.',
+      ),
+    );
+  });
 });
