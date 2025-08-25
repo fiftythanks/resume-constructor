4e0a5d6a47f56745288a27a42f3d3d4bc7a9ede0
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-08-25 18:15:38 +0700

    test(components): refactor `Button` tests to TS

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,46 @@
+import React, { MouseEvent } from 'react';
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import Button from './Button';
+
+describe('Button', () => {
+  let handleClickMock: jest.Mock<void, [MouseEvent<HTMLButtonElement>]>;
+
+  beforeEach(() => {
+    handleClickMock = jest.fn();
+  });
+
+  it('should render with the correct accessible name from its children', () => {
+    render(<Button onClick={handleClickMock}>Click me</Button>);
+
+    const btn = screen.getByRole('button', { name: /click me/i });
+
+    expect(btn).toBeInTheDocument();
+  });
+
+  it('should use the `aria-label` prop for its accessible name when provided', () => {
+    render(
+      <Button aria-label="Close Dialog" onClick={handleClickMock}>
+        X
+      </Button>,
+    );
+
+    const btn = screen.getByRole('button', { name: /close dialog/i });
+
+    expect(btn).toBeInTheDocument();
+    expect(btn).toHaveTextContent('X');
+  });
+
+  it('should call the `onClick` handler when the user clicks the button', async () => {
+    const user = userEvent.setup();
+    render(<Button onClick={handleClickMock}>Click me</Button>);
+    const btn = screen.getByRole('button', { name: /click me/i });
+
+    await user.click(btn);
+
+    expect(handleClickMock).toHaveBeenCalledTimes(1);
+  });
+});
