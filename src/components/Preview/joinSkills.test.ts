73afaf71cfa24c1c7a33cee1a429c42abaf6b11c
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-11-05 16:19:44 +0500

    refactor(components): improve parameter's type
    I declared `joinSkills`'s parameter `ReadonlyDeep` and gave it a proper
    `Skills[k]` type.

--- src/components/Preview/joinSkills.test.ts
+++ src/components/Preview/joinSkills.test.ts
@@ -1,11 +1,13 @@
 import joinSkills from './joinSkills';
 
+import { ItemWithId } from '@/types/resumeData';
+
 describe('joinSkills()', () => {
   it('should transform an array of skill objects into a single, comma-separated string', () => {
-    const skills = [
-      { value: 'string' },
-      { value: 'another string' },
-      { value: 'third' },
+    const skills: ItemWithId[] = [
+      { value: 'string', id: '5-5-5-5-5' },
+      { value: 'another string', id: '5-5-5-5-5' },
+      { value: 'third', id: '5-5-5-5-5' },
     ];
 
     expect(joinSkills(skills)).toBe('string, another string, third');
--- src/components/Preview/joinSkills.ts
+++ src/components/Preview/joinSkills.ts
@@ -1,9 +1,15 @@
+import { ReadonlyDeep } from 'type-fest';
+
+import type { Skills } from '@/types/resumeData';
+
 /**
  * Transforms an array of skill objects into a single, comma-separated string.
  *
  * @param skills The array of skill objects to join.
  * @returns A string of skill values.
  */
-export default function joinSkills(skills: { value: string }[]): string {
+export default function joinSkills<K extends keyof Skills>(
+  skills: ReadonlyDeep<Skills[K]>,
+): string {
   return skills.map((item) => item.value).join(', ');
 }
