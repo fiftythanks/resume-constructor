97c8ac888024f3a72a8cfff1ea8a5febb365ad82
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-11-09 13:08:56 +0500

    refactor(preview): rewrite `ResumeSection` to TS

--- src/components/Preview/ResumeSection.tsx
+++ src/components/Preview/ResumeSection.tsx
@@ -2,53 +2,122 @@ import React from 'react';
 
 import { Link, StyleSheet, Text, View } from '@react-pdf/renderer';
 
+import neverReached from '@/utils/neverReached';
+
 import joinSkills from './joinSkills';
+
+import type { ResumeData } from '@/types/resumeData';
+import type { ViewProps } from '@react-pdf/renderer';
+import type { ReadonlyDeep } from 'type-fest';
 
 const styles = StyleSheet.create({
   bold: {
     fontWeight: 'bold',
   },
+
   bulletPoint: {
     display: 'flex',
     flexDirection: 'row',
     gap: 5,
-    alignItems: 'first baseline',
+
+    // TODO: it was `first baseline` but `alignItems` can't be `first baseline`, so I changed it to `baseline`. Now it has to be checked if something's broken.
+    alignItems: 'baseline',
   },
+
   italic: {
     fontStyle: 'italic',
   },
+
   jobHeader: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+
   rightJobHeaderPart: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-end',
   },
+
   job: {
     marginTop: 5,
   },
+
   project: {
     marginTop: 5,
   },
+
   degreeHeader: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+
   rightDegreeHeaderPart: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-end',
   },
+
   degree: {
     marginTop: 5,
   },
 });
 
-export default function ResumeSection({ data, sectionName, style }) {
+interface BaseProps {
+  style?: ViewProps['style'];
+}
+
+interface CertificationsProps {
+  data: ResumeData['certifications'];
+  sectionName: 'certifications';
+}
+
+interface EducationProps {
+  data: ResumeData['education'];
+  sectionName: 'education';
+}
+
+interface ExperienceProps {
+  data: ResumeData['experience'];
+  sectionName: 'experience';
+}
+
+interface ProjectsProps {
+  data: ResumeData['projects'];
+  sectionName: 'projects';
+}
+
+interface SkillsProps {
+  data: ResumeData['skills'];
+  sectionName: 'skills';
+}
+
+/**
+ * Not for export. If you need to export `ResumeSectionProps`, create another
+ * type without `ReadonlyDeep`.
+ *
+ * The union looks so awkward with all these `ReadonlyDeep`, and it's
+ * intentional. The thing is, if you "ReadonlyDeep" the entire union, you can't
+ * later access `data.certificates` etc. as intended - TypeScript won't be able
+ * to infer the correct type.
+ */
+type ResumeSectionProps = BaseProps &
+  (
+    | ReadonlyDeep<CertificationsProps>
+    | ReadonlyDeep<EducationProps>
+    | ReadonlyDeep<ExperienceProps>
+    | ReadonlyDeep<ProjectsProps>
+    | ReadonlyDeep<SkillsProps>
+  );
+
+//? Why is it named `sectionName` here, whereas it's `sectionId` absolutely everywhere else? It shouldn't be like that, should it?
+export default function ResumeSection({
+  data,
+  sectionName,
+  style,
+}: ResumeSectionProps) {
   // Not using it by default for all sections and fields isn't the most user-friendly approach.
   // TODO: examine where you need conditional rendering and where you don't.
   switch (sectionName) {
@@ -58,12 +127,14 @@ export default function ResumeSection({ data, sectionName, style }) {
           <Text style={styles.bold}>CERTIFICATES, SKILLS & INTERESTS</Text>
           <View style={style}>
             <Text
-              render={() => (
-                <>
-                  <Text style={styles.bold}>Certificates:</Text>{' '}
-                  {data.certificates}
-                </>
-              )}
+              render={() => {
+                return (
+                  <>
+                    <Text style={styles.bold}>Certificates:</Text>{' '}
+                    {data.certificates}
+                  </>
+                );
+              }}
             />
             <Text
               render={() => (
@@ -92,7 +163,10 @@ export default function ResumeSection({ data, sectionName, style }) {
             render={() => (
               <>
                 {data.degrees.map((degree, i) => (
-                  <View key={degree.id} style={i > 0 && styles.degree}>
+                  <View
+                    key={degree.id}
+                    style={i > 0 ? styles.degree : undefined}
+                  >
                     <View style={styles.degreeHeader}>
                       <View>
                         <Text
@@ -144,7 +218,7 @@ export default function ResumeSection({ data, sectionName, style }) {
             render={() => (
               <>
                 {data.jobs.map((job, i) => (
-                  <View key={job.id} style={i > 0 && styles.job}>
+                  <View key={job.id} style={i > 0 ? styles.job : undefined}>
                     <View style={styles.jobHeader}>
                       <View>
                         <Text
@@ -198,7 +272,7 @@ export default function ResumeSection({ data, sectionName, style }) {
                 {data.projects.map((project, i) => (
                   <View
                     key={project.id}
-                    style={i > 0 && styles.project}
+                    style={i > 0 ? styles.project : undefined}
                     render={() => (
                       <>
                         <Text
@@ -296,6 +370,6 @@ export default function ResumeSection({ data, sectionName, style }) {
       );
     }
     default:
-    // Do nothing.
+      neverReached(sectionName);
   }
 }
