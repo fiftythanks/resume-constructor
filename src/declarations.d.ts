66e518a14b92faf5597e81c65d07e642abed4d24
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-11-09 14:53:21 +0500

    refactor(types): add declaration for pdfjs module

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -9,3 +9,6 @@ declare module '*.ttf' {
   const content: string;
   export default content;
 }
+
+// A fix for a "Cannot find module 'pdfjs-dist/webpack' or its corresponding type declarations" error when importing from "pdfjs-dist/webpack".
+declare module 'pdfjs-dist/webpack';
