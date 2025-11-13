8ba1dce920a3a100884213c1414d7a370ff1abdf
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-11-13 16:21:48 +0500

    refactor(skills): rewrite `Skills` to TypeScript

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import useAppState from '@/hooks/useAppState';
+import useResumeData from '@/hooks/useResumeData';
+
 import BulletPoints from '@/components/BulletPoints';
+
+import type { ResumeData } from '@/types/resumeData';
+import type { ReadonlyDeep } from 'type-fest';
+
+export interface SkillsProps {
+  data: ResumeData['skills'];
+  functions: ReturnType<typeof useResumeData>['skillsFunctions'];
+
+  updateScreenReaderAnnouncement: ReturnType<
+    typeof useAppState
+  >['updateScreenReaderAnnouncement'];
+}
 
 export default function Skills({
   data,
   functions,
   updateScreenReaderAnnouncement,
-}) {
+}: ReadonlyDeep<SkillsProps>) {
   // Skills don't need to be bullet points. I'd say they must not be bullet points at all. They are single-line. Why on earth are they bullet points? It's strange and super redundant. For each line it should be a simple input field, and that's all.
   // TODO: switch to simple input fields from redundant bullet points.
   return (
