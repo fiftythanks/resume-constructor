557a93826c01fc183d7d41499985e0e4ca87fd20
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-11-15 22:31:11 +0500

    test(personal): improve `Personal` coverage

--- src/pages/Personal/Personal.test.tsx
+++ src/pages/Personal/Personal.test.tsx
@@ -56,7 +56,28 @@ describe('Personal', () => {
       expect(input).toBeInTheDocument();
     });
 
-    it('should call `updatePersonal` when Full Name is changed', async () => {
+    it('should have a placeholder "John Doe" for Full Name', () => {
+      render(<Personal {...getProps()} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Full Name',
+      });
+
+      expect(input.placeholder).toBe('John Doe');
+    });
+
+    it('should pass the correct value to the text input from `data`', () => {
+      const props = getProps();
+      render(<Personal {...props} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Full Name',
+      });
+
+      expect(input.value).toBe(props.data.fullName);
+    });
+
+    it("should call `updatePersonal` when the text input's value is changed", async () => {
       const updatePersonalMock = jest.fn(
         (
           _field:
@@ -84,7 +105,7 @@ describe('Personal', () => {
       await user.type(input, 's');
 
       expect(updatePersonalMock).toHaveBeenCalledTimes(1);
-    });
+    }, 10000);
   });
 
   describe('Job Title', () => {
@@ -96,7 +117,28 @@ describe('Personal', () => {
       expect(input).toBeInTheDocument();
     });
 
-    it('should call `updatePersonal` when Job Title is changed', async () => {
+    it('should have a placeholder "Frontend Engineer" for Job Title', () => {
+      render(<Personal {...getProps()} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Job Title',
+      });
+
+      expect(input.placeholder).toBe('Frontend Engineer');
+    });
+
+    it('should pass the correct value to the text input from `data`', () => {
+      const props = getProps();
+      render(<Personal {...props} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Job Title',
+      });
+
+      expect(input.value).toBe(props.data.jobTitle);
+    });
+
+    it("should call `updatePersonal` when the text input's value is changed", async () => {
       const updatePersonalMock = jest.fn(
         (
           _field:
@@ -136,7 +178,28 @@ describe('Personal', () => {
       expect(input).toBeInTheDocument();
     });
 
-    it('should call `updatePersonal` when Email is changed', async () => {
+    it('should have a placeholder "john.doe@gmail.com" for Email', () => {
+      render(<Personal {...getProps()} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Email',
+      });
+
+      expect(input.placeholder).toBe('john.doe@gmail.com');
+    });
+
+    it('should pass the correct value to the text input from `data`', () => {
+      const props = getProps();
+      render(<Personal {...props} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Email',
+      });
+
+      expect(input.value).toBe(props.data.email);
+    });
+
+    it("should call `updatePersonal` when the text input's value is changed", async () => {
       const updatePersonalMock = jest.fn(
         (
           _field:
@@ -176,7 +239,28 @@ describe('Personal', () => {
       expect(input).toBeInTheDocument();
     });
 
-    it('should call `updatePersonal` when Phone is changed', async () => {
+    it('should have a placeholder "+7 666 534-32-33" for Phone', () => {
+      render(<Personal {...getProps()} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Phone',
+      });
+
+      expect(input.placeholder).toBe('+7 666 534-32-33');
+    });
+
+    it('should pass the correct value to the text input from `data`', () => {
+      const props = getProps();
+      render(<Personal {...props} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Phone',
+      });
+
+      expect(input.value).toBe(props.data.phone);
+    });
+
+    it("should call `updatePersonal` when the text input's value is changed", async () => {
       const updatePersonalMock = jest.fn(
         (
           _field:
@@ -216,7 +300,28 @@ describe('Personal', () => {
       expect(input).toBeInTheDocument();
     });
 
-    it('should call `updatePersonal` when Address is changed', async () => {
+    it('should have a placeholder "Cool St, Cambridge" for Address', () => {
+      render(<Personal {...getProps()} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Address',
+      });
+
+      expect(input.placeholder).toBe('Cool St, Cambridge');
+    });
+
+    it('should pass the correct value to the text input from `data`', () => {
+      const props = getProps();
+      render(<Personal {...getProps()} />);
+
+      const input: HTMLInputElement = screen.getByRole('textbox', {
+        name: 'Address',
+      });
+
+      expect(input.value).toBe(props.data.address);
+    });
+
+    it("should call `updatePersonal` when the text input's value is changed", async () => {
       const updatePersonalMock = jest.fn(
         (
           _field:
@@ -248,15 +353,38 @@ describe('Personal', () => {
   });
 
   describe('Summary', () => {
-    it('should render a text input for Summary', () => {
+    it('should render a textarea for Summary', () => {
       render(<Personal {...getProps()} />);
 
-      const input = screen.getByRole('textbox', { name: 'Summary' });
+      const textarea = screen.getByRole('textbox', { name: 'Summary' });
 
-      expect(input).toBeInTheDocument();
+      expect(textarea).toBeInTheDocument();
     });
 
-    it('should call `updatePersonal` when Summary is changed', async () => {
+    it('should have a placeholder "Detail-oriented Frontend Engineer eager to create seamless user experiences." for Summary', () => {
+      render(<Personal {...getProps()} />);
+
+      const textarea: HTMLTextAreaElement = screen.getByRole('textbox', {
+        name: 'Summary',
+      });
+
+      expect(textarea.placeholder).toBe(
+        'Detail-oriented Frontend Engineer eager to create seamless user experiences.',
+      );
+    });
+
+    it('should pass the correct value to the textarea from `data`', () => {
+      const props = getProps();
+      render(<Personal {...props} />);
+
+      const textarea: HTMLTextAreaElement = screen.getByRole('textbox', {
+        name: 'Summary',
+      });
+
+      expect(textarea.value).toBe(props.data.summary);
+    });
+
+    it("should call `updatePersonal` when the textarea's value is changed", async () => {
       const updatePersonalMock = jest.fn(
         (
           _field:
@@ -279,9 +407,9 @@ describe('Personal', () => {
       );
 
       const user = userEvent.setup();
-      const input = screen.getByRole('textbox', { name: 'Summary' });
+      const textarea = screen.getByRole('textbox', { name: 'Summary' });
 
-      await user.type(input, 's');
+      await user.type(textarea, 's');
 
       expect(updatePersonalMock).toHaveBeenCalledTimes(1);
     });
