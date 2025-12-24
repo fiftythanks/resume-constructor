02d360438bdf508fe7da35989e9a42eaedc70ccf
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-12-24 17:26:20 +0500

    chore(jest): ignore `/e2e/` when running tests

--- jest.config.ts
+++ jest.config.ts
@@ -189,7 +189,7 @@ const jestConfig: JestConfigWithTsJest = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/e2e/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.m?[jt]sx?$',
